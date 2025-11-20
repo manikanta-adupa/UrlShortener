a0bceedcb60e3466817aeed54e1a014014a51c1d
manikanta-adupa <manikantapradeep.adapa@gmail.com>
2025-11-20 23:48:46 +0530

    Change

---
+++ src/main/java/com/learning/urlshortener/handler/LambdaHandler.java
@@ -0,0 +1,30 @@
+package com.learning.urlshortener.handler;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.learning.urlshortener.UrlShortenerApplication;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class LambdaHandler implements RequestStreamHandler {
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+    static {
+        try{
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(UrlShortenerApplication.class);
+        }catch(ContainerInitializationException e){
+            e.printStackTrace();
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+    }
+}
